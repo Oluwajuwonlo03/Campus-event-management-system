17af959209771b9daeeef22ffdea5ae9e6f75c6b
Oluwajuwonlo03 <igejuwon06@gmail.com>
2026-09-04 09:42:43 +0100

    Added explore format and calls for papers sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import ExploreFormat from "./components/ExploreFormat";
 import FeaturedEvents from "./components/FeaturedEvents";
+import CallsForPapers from "./components/CallsForPapers";
 import ExploreEvents from "./components/ExploreEvents";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <ExploreFormat />
       <FeaturedEvents />
+      <CallsForPapers />
       <ExploreEvents/>
     </>
   );
